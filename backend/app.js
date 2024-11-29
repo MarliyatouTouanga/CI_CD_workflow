b51ca4b90b75f88d821da23a0c566e71b27d91af
MarliyatouTouanga <touangamarliyatou>
2024-11-29 14:20:47 +0100

    Adding a comment as new feature

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+/*adding my own uri*/
 const mongoURI = process.env.MONGO_URL || "mongodb+srv://touangamarliyatou:Lysandre1@cluster0.qqmuv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
